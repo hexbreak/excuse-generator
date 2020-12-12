0a183618b71270794c60aedf5ed7b1906446bc48
hexbreak <theownerproject@gmail.com>
2020-12-12 05:17:22 +0000

    Updates

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ import "./style.css";
 window.onload = () => {
   let randomNumber = Math.random() * 10;
   document.querySelector("#excuse").innerHTML = generateExcuse();
-  console.log("Hello Rigo from the console! ");
 };
 
 let generateExcuse = () => {
